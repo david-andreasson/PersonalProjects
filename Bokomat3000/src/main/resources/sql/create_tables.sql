67998a6b0cd15698174271f6c9cc542aea23a637
david-andreasson <79davand@gafe.molndal.se>
2024-11-09 05:16:26 +0100

    Add initial SQL schema for User table
    - Created User table with fields for name, apartment number, email, phone number, and password hash.
    - Added unique constraints on apartment number and email for future login functionality.

---
+++ Bokomat3000/src/main/resources/sql/create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS `User` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(45) NOT NULL,
+    `apartmentNumber` VARCHAR(45) NOT NULL,
+    `email` VARCHAR(45) NOT NULL,
+    `phonenumber` VARCHAR(45) NOT NULL,
+    `password_hash` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `apartmentNumber_UNIQUE` (`apartmentNumber` ASC),
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC)
+    );
+
+CREATE TABLE IF NOT EXISTS `Booking` (
+
