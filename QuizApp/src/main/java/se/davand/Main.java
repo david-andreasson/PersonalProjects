2da39b67b0c9a5c8e883b6fb3eb7c04e789748fc
david-andreasson <79davand@gafe.molndal.se>
2024-12-06 04:24:41 +0100

    New project - QuizApp to use for studying

---
+++ QuizApp/src/main/java/se/davand/Main.java
@@ -0,0 +1,28 @@
+package se.davand;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //Ska vi spara frågorna i en databas?
+        //isf kan jag utöka appen och lägga till nya frågor när vi går mer kurser
+        //och i menyn skulle man kunna välja vilken kurs man vill quizza sig själv på.
+
+        //Funktioner:
+        // Läsa in frågor och svar från SQLite-databas.
+        //Presentera frågor och svarsalternativ för användaren.
+        //Ta emot användarens svar och jämföra det med det rätta svaret.
+        //Visa antal rätt/felprocent efter varje fråga. (personligt val)
+
+        //Todo - gör mappstruktur
+        //Todo - klass för databas
+        //Todo - klass för meny
+        //Todo - klass för frågor och svar
+        //Todo - klass för logik
+        //Todo - metod för att räkna ut felprocent
+
+
+
+
+    }
+}
