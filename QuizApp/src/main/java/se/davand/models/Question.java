ee126655cef2927c3d2d80043707de459c4dc261
david-andreasson <79davand@gafe.molndal.se>
2024-12-06 06:25:30 +0100

    - Add model and method Question

--- QuizApp/src/main/java/se/davand/models/Question.java
+++ QuizApp/src/main/java/se/davand/models/Question.java
@@ -1,4 +1,32 @@
 package se.davand.models;
 
+import java.util.List;
+
 public class Question {
+    private final String questionText;
+    private final List<String> options;
+    private final String correctOption;
+
+    public Question(String questionText, List<String> options, String correctOption) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctOption = correctOption;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public String getCorrectOption() {
+        return correctOption;
+    }
+
+    public boolean isCorrect(String userAnswer) {
+        return correctOption.equalsIgnoreCase(userAnswer);
+    }
 }
+
